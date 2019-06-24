75f04d28e463a13a06f5bfa9dd52533b0bc72718
Jonathan Wong <jon.wong92@gmail.com>
2019-06-24 15:51:49 -0700

    Bug 3

--- imports/startup/server/main.js
+++ imports/startup/server/main.js
@@ -23,6 +23,6 @@ Meteor.methods({
        Teams.insert(post, callback)
    },
     findTeams: function(post){
-       return Teams.find(post).fetch()
+       return Teams.findOne(post).fetch()
     }
 });
